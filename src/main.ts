a31b061e5b5183e47fb2b400fd4c3b41df6b9cb5
alexandr <alexandrpastushkov@gmail.com>
2015-07-13 10:00:24 +0300

    clear

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@
 /// <reference path="./view/listView"/>
 /// <reference path="./dispatcher/dispatcher"/>
 
-var listItem = new ListItem('wrgrtgrt','q');
-
 var dispatcher:Dispatcher = new Dispatcher();
 
 var list = new List(dispatcher, new ListView(dispatcher,
